5f02883bdc49510d4e7a323b61026c52d3622aba
Nishan Bista <lw.nishan@gmail.com>
2025-12-06 19:28:29 +0545

    mongodb database connection

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,15 @@
-import app from "./app"
-import config from "./config/config"
+import app from "./app";
+import config from "./config/config";
+import { connectMongoDB } from "./utils";
 
-app.listen(config.port, () => {
-    console.log(
-        `SERVER RUNNING ON PORT: ${config.port}\nENVIRONMENT: ${config.nodeEnv}`,
-    );
-});
-
-
+connectMongoDB()
+    .then(() => {
+        app.listen(config.port, () => {
+            console.log(
+                `SERVER RUNNING ON PORT: ${config.port}\nENVIRONMENT: ${config.nodeEnv}`
+            );
+        });
+    })
+    .catch((error) => {
+        console.log(`SERVER FAILED TO RUN \nERROR: ${error}`);
+    });
--- server/src/utils/index.ts
+++ server/src/utils/index.ts
@@ -1,9 +1,6 @@
 import asyncHandler from "./async_handler";
 import ApiSuccessResponse from "./ApiSuccessResponse";
 import ApiErrorResponse from "./ApiErrorResponse";
+import connectMongoDB from "./connect_mongodb";
 
-export {
-    asyncHandler,
-    ApiSuccessResponse,
-    ApiErrorResponse
-}
+export { asyncHandler, ApiSuccessResponse, ApiErrorResponse, connectMongoDB };
