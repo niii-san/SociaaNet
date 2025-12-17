65f4a045a0974ff949baf80ffb18bb02caee6e5f
Nishan Bista <lw.nishan@gmail.com>
2025-12-17 17:53:13 +0545

    client changed to nextjs

---
+++ client/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+    return (
+        <div className="">
+            <h1 className="text-4xl font-bold">
+                Welcome to SociaaNet
+            </h1>
+        </div>
+    );
+}
