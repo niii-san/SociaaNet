d1414bbc9ea4ab7f44473048bd04e90acff5c405
Nishan Bista <lw.nishan@gmail.com>
2025-12-27 22:30:36 +0545

    color theme added

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <div className="">
-            <h1 className="text-4xl font-bold">
+            <h1 className="text-4xl font-bold bg-primary">
                 Welcome to SociaaNet
             </h1>
         </div>
