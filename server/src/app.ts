fef1f00cc3141d26ec163ef4e2ac8c84eb0732c8
Nishan Bista <lw.nishan@gmail.com>
2026-01-21 22:02:31 +0545

    feat: signup

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,20 @@
 import express, { Request, Response } from "express";
 import cookieParser from "cookie-parser";
 import { ApiSuccessResponse } from "./utils";
+import cors from "cors";
 
 const app = express();
 // config
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+// cors settings
+const corsOption = {
+    origin: "http://localhost:3000"
+};
+app.use(cors(corsOption));
 
 app.get("/", (_: Request, res: Response) => {
     return res
@@ -18,11 +26,8 @@ app.get("/", (_: Request, res: Response) => {
 import { authRouter } from "./routes";
 app.use("/api/v1/auth", authRouter);
 
-
-
 // Error handling middleware
 import { errorMiddleware } from "./utils";
-app.use(errorMiddleware)
-
+app.use(errorMiddleware);
 
 export default app;
