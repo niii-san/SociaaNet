aead1c63696895d0a7efa63f984d34438d877ec2
Nishan Bista <lw.nishan@gmail.com>
2026-01-31 15:53:22 +0545

    feat: logger added on server

--- server/src/utils/index.ts
+++ server/src/utils/index.ts
@@ -3,12 +3,14 @@ import connectMongoDB from "./connect_mongodb";
 import generateUniqueUsername from "./generate_unique_username";
 import ApiSuccessResponse from "./ApiSuccessResponse";
 import ApiErrorResponse, { errorMiddleware } from "./ApiErrorResponse";
+import { logger } from "./logger";
 
 export {
-  asyncHandler,
-  ApiSuccessResponse,
-  ApiErrorResponse,
-  connectMongoDB,
-  errorMiddleware,
-  generateUniqueUsername
+    asyncHandler,
+    ApiSuccessResponse,
+    ApiErrorResponse,
+    connectMongoDB,
+    errorMiddleware,
+    generateUniqueUsername,
+    logger
 };
