482f43b544dfc18e53f26a70f7aa65f6ebec4dc6
Nishan Bista <lw.nishan@gmail.com>
2026-03-01 05:56:37 +0545

    fix: use correct user ID in feed controllers, fix suggested users showing followed/own accounts

--- server/src/controllers/feed/get-home-feed.controller.ts
+++ server/src/controllers/feed/get-home-feed.controller.ts
@@ -1,10 +1,11 @@
-import { Request, Response } from "express";
+import { Response } from "express";
 import { asyncHandler, HttpSuccess } from "../../utils";
 import { feedService } from "../../services/feed.service";
+import { RequestWithUserContext } from "../../types";
 
 export const getHomeFeedController = asyncHandler(
-    async (req: Request, res: Response) => {
-        const userId = (req as any).userId;
+    async (req: RequestWithUserContext, res: Response) => {
+        const userId = req.user._id.toString();
         const page = parseInt(req.query.page as string) || 1;
         const limit = Math.min(
             parseInt(req.query.limit as string) || 10,
--- server/src/controllers/feed/get-suggested-users.controller.ts
+++ server/src/controllers/feed/get-suggested-users.controller.ts
@@ -1,10 +1,11 @@
-import { Request, Response } from "express";
+import { Response } from "express";
 import { asyncHandler, HttpSuccess } from "../../utils";
 import { feedService } from "../../services/feed.service";
+import { RequestWithUserContext } from "../../types";
 
 export const getSuggestedUsersController = asyncHandler(
-    async (req: Request, res: Response) => {
-        const userId = (req as any).userId;
+    async (req: RequestWithUserContext, res: Response) => {
+        const userId = req.user._id.toString();
         const limit = Math.min(
             parseInt(req.query.limit as string) || 5,
             20
